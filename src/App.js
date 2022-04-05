98a63cceb4b6bdc8ea4a10230aa092e8f421d478
Taisei-rgb <69156263+Taisei-rgb@users.noreply.github.com>
2022-04-05 22:45:21 +0900

    ドラッグアンドドロップ機能の実装 (#2)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
-function App() {
-	return <div></div>;
-}
+import { ITEMS } from "./itemsData";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-export default App;
+const grid = 8;
+
+const getListStyle = (isDraggingOver) => ({
+	background: isDraggingOver ? "lightblue" : "lightgray",
+	width: 250,
+	padding: grid,
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+	userSelect: "none",
+	padding: grid * 2,
+	margin: `0 0 ${grid} 0`,
+	background: isDragging ? "lightgreen" : "grey",
+
+	...draggableStyle,
+});
+
+const reorder = (list, startIndex, endIndex) => {
+	const removed = list.splice(startIndex, 1);
+	list.splice(endIndex, 0, removed[0]);
+};
+
+export const App = () => {
+	const onDragEnd = (result) => {
+		if (!result.destination) {
+			return;
+		}
+		reorder(ITEMS, result.source.index, result.destination.index);
+	};
+	return (
+		<div className="items-center">
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Droppable droppableId="droppable">
+					{(provided, snapshot) => (
+						<div
+							{...provided.droppableProps}
+							ref={provided.innerRef}
+							style={getListStyle(snapshot.isDraggingOver)}
+						>
+							{ITEMS.map((item, index) => (
+								<Draggable key={item.id} draggableId={item.id} index={index}>
+									{(provided, snapshot) => (
+										<div
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+											style={getItemStyle(
+												snapshot.isDragging,
+												provided.draggableProps.style
+											)}
+										>
+											{item.content}
+										</div>
+									)}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+		</div>
+	);
+};
---
+++ src/itemsData.js
@@ -0,0 +1,22 @@
+export const ITEMS = [
+	{
+		id: "item-0",
+		content: "item 0",
+	},
+	{
+		id: "item-1",
+		content: "item 1",
+	},
+	{
+		id: "item-2",
+		content: "item 2",
+	},
+	{
+		id: "item-3",
+		content: "item 3",
+	},
+	{
+		id: "item-4",
+		content: "item 4",
+	},
+];
